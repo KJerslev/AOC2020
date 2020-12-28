a37549cbc7d81fd975f107919ab693b4362e0582
Equilibrium <equilibrium@users.noreply.github.com>
2020-12-28 17:11:38 +0100

    Problem 9 part 2 solved

--- 09/9.py
+++ 09/9.py
@@ -7,31 +7,40 @@ with open('input.dat', 'r') as fd:
     numbers = [int(line.rstrip()) for line in fd]
 
 preamble = 25        # How many numbers to be ignored
-
-# Part 1
+FirstInvalid = 0
+FirstInvalidPos = 0
 
 
 def check_number(pos):
-    global numbers
+    global FirstInvalid, FirstInvalidPos
     startpos = pos - preamble
     combs = [lst for lst in combinations(numbers[startpos:pos], 2)]     # Create list of unique combinations of numbers
     sums = [sum(i) for i in combs]
     if numbers[pos] in sums:
         check_number(pos + 1)
     else:
+        FirstInvalid = numbers[pos]
+        FirstInvalidPos = pos
         print("Part 1: " + str(numbers[pos]) + " is the first number not equal to a sum")
-    return [numbers[pos], pos]
 
 
-FirstInvalidLst = check_number(preamble)
+check_number(preamble)
 
-# # Part 2
-#
-# FirstInvalid = FirstInvalidLst[0]
-# FirstInvalidPosition = FirstInvalidLst[1]
-# ListOfNumbers = numbers[:FirstInvalidPosition]
-#
-# def check_sum(length):
-#     for i in range(2,FirstInvalidPosition):
-#         combs2 = [lst for lst in combinations(ListOfNumbers, i)]
-#
+# Part 2
+
+ListOfNumbers = numbers[:FirstInvalidPos]
+
+
+def get_contiguous_lst():
+    for i in range(0, FirstInvalidPos):
+        for j in range(i, FirstInvalidPos):
+            contiguousList = numbers[i:j]
+            if sum(contiguousList) == FirstInvalid:
+                return contiguousList
+
+
+Part2Lst = get_contiguous_lst()
+
+print("Part 2: The minimum value in the contiguous list is: " + str(min(Part2Lst)))
+print("Part 2: The maximum value in the contiguous list is: " + str(max(Part2Lst)))
+print("Part 2: Their sum is: " + str(min(Part2Lst) + max(Part2Lst)))
